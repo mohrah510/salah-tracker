ce58c2337298841b57608af1ec9cef922a0bbb01
Mohammed Rahman <147890223+mohrah510@users.noreply.github.com>
2026-08-23 11:53:47 -0700

    Initialize Spring Boot backend with JPA, Security, Postgres dependencies

---
+++ backend/src/test/java/com/salahtracker/salah_tracker_backend/SalahTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salahtracker.salah_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalahTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
