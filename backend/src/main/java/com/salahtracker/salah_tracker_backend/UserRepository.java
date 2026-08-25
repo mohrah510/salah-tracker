989a5fd7431e12706201447935b68e8a01c70e22
Mohammed Rahman <147890223+mohrah510@users.noreply.github.com>
2026-08-25 09:26:15 -0700

    Add User entity and repository

---
+++ backend/src/main/java/com/salahtracker/salah_tracker_backend/UserRepository.java
@@ -0,0 +1,10 @@
+package com.salahtracker.salah_tracker_backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
